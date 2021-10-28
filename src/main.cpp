b1badc5a7a974d27b761dbf8c5ce2a6ebf01d736
Giovanni Giacalone <gbgiacalone@wpi.edu>
2021-10-28 14:03:35 -0400

    Pushing code for hc-sr04 modifications

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
-/*
- * Code for interfacing with the MCP3002 ADC using the SPI bus on the ESP32.
- * This code is written to use specific pins:
- * 
- * MISO = 19
- * MOSI = 23
- * SCLK = 18
- * SS   = 5
- * 
- * RX   = 16 (inverted for MaxBotix!)
- * (best not to use TX-17 for anything else, since the uart will likely conflict with pin operation)
- * 
- * pulse width is read on: 35
- * ultrasonic control pin: 2
- */
 
 #include <Arduino.h>
-#include <SPI.h>
+#include <HC-SR04.h>
 
-#include <MaxBotix.h>
+const int Trig = 16;
+const int Echo = 17;
+
+Rangefinder rangefinder;
 
 void setup()
 {
@@ -26,37 +14,11 @@ void setup()
   Serial.begin(115200);
   Serial.println("Velkommen til");
 
-  mb_ez1.init();
+  rangefinder.attach(Trig, Echo);
 }
 
 void loop() 
 {
-  /**
-   * For this demo, we key everything on the ascii output, since
-   * that is the last thing that the sensor prepares for output.
-   * Everything else should be ready at that point.
-   */
-  uint16_t asciiResponse = mb_ez1.readASCII();
-  
-  if(asciiResponse) 
-  {
-    Serial.print(asciiResponse);
-    Serial.print('\t');
-    Serial.print(0); //TODO: change this line to output distance in cm
-    Serial.print('\t');
-
-    uint32_t pulseLen = mb_ez1.checkEcho();
-    Serial.print(pulseLen);
-    Serial.print('\t');
-    Serial.print(0); //TODO: change this line to output distance in cm
-    Serial.print('\t');
-
-    //passing true ignores the timer and forces a reading
-    //from the datasheet, if the serial output is ready, the voltage is ready
-    uint16_t adcReading = mb_ez1.readMCP3002(true);
-    Serial.print(adcReading); 
-    Serial.print('\t');
-    Serial.print(0); //TODO: change this line to output distance in cm
-    Serial.print('\n');
-  }
+  Serial.print("Distance: ");
+  Serial.print(rangefinder.getDistanceCM());
 }
