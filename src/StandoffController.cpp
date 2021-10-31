f757115eb71a5a57c82536e998900db599e7e8ff
Giovanni Giacalone <gbgiacalone@wpi.edu>
2021-10-30 21:08:28 -0400

    Everything should work fine but chassis doesnt move.

--- src/StandoffController.cpp
+++ src/StandoffController.cpp
@@ -2,9 +2,9 @@
 
 StandoffController::StandoffController(float Kp): pid(Kp) {};
 
-void StandoffController::setTarget(float target) {
-    pid.setTarget(target);
-}
+// void StandoffController::setTarget(float target) {
+//     pid.setTarget(target);
+// }
 
 void StandoffController::process(float current) {
     leftEffort = rightEffort = pid.CalcEffort(current);
--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,36 @@
 #include "StandoffController.h"
 #include "Chassis.h"
 #include "Filters.h"
+#include "IRDecoder.h"
+
+const float KP = 2;
+const float KI = 0.0;
+const float KD = 0.0;
 
 float dist = 0;
-StandoffController standoff = StandoffController(2);
+// StandoffController standoff = StandoffController(2);
 AvgFilter<float, 5> ultraFilter;
+float setPoint = 20;
+float error;
+float integral = 0;
+float deltaError = 0;
+float speed = 0;
 
+IRDecoder decoder(15);
+
+//IR Codes
+const int Forward = -100286720;
+const int Backward = -233980160;
+const int Stop = -167133440;
+int keyPressed = 0;
+int lastKeyPressed = 0;
 
 void setup()
 {
   chassis.init();
   hcsr_1.init();
-  //ir_1.init();
-  pinMode(0, INPUT_PULLUP);
+  decoder.init();
   delay(1000);
-  standoff.setTarget(20);
 
   Serial.begin(115200);
   Serial.println("Velkommen til");
@@ -30,8 +46,26 @@ void loop() {
     ultraFilter.addDatum(dist);
     //standoff.process(ultraFilter.getMedian());
     //chassis.setMotorEfforts(-standoff.l(), -standoff.r());
-    Serial.println(ultraFilter.getMedian());
+    //Serial.println(ultraFilter.getMedian());
   }
+  keyPressed = decoder.getCode();
+  if (keyPressed == Forward && lastKeyPressed != Forward && setPoint >= 20) {
+    setPoint -= 10;
+  }
+  if (keyPressed == Backward && lastKeyPressed != Backward && setPoint <= 100) {
+    setPoint += 10;
+  }
+  if (keyPressed == Stop) {
+    chassis.stop();
+  }
+  //Serial.print("Setpoint: ");
+  //Serial.println(setPoint);
+  error = dist - setPoint;
+  Serial.println(error);
+  speed = error * KP + integral * KI +  deltaError * KD;
+  chassis.setMotorEfforts(speed, speed);
+  delay(50);
+
 }
 
 /*
